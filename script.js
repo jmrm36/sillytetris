7d972d5b0454491d055af6b67fcae7af47e2f02d
jmrm36 <79798928+jmrm36@users.noreply.github.com>
2025-09-08 08:26:34 -0400

    fix: prevent iOS Safari zoom

--- script.js
+++ script.js
@@ -250,3 +250,13 @@ const colors = [
   '#ffe138',
   '#3877ff',
 ];
+
+// Fallback to prevent iOS Safari double-tap zoom
+let lastTouchEnd = 0;
+document.addEventListener('touchend', (event) => {
+  const now = Date.now();
+  if (now - lastTouchEnd <= 300) {
+    event.preventDefault();
+  }
+  lastTouchEnd = now;
+}, false);
